ebfc192c57ca4d631df89d42b039bba2fbdc7d1b
Exudonze <145332493+Exudonze@users.noreply.github.com>
2023-09-18 11:25:46 +0200

    WetterappORIGIN

--- App/WetterApp/WetterApp/MainWindow.xaml.cs
+++ App/WetterApp/WetterApp/MainWindow.xaml.cs
@@ -1,30 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
 
 namespace WetterApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private const string apiKey = "79270c12757b499a9d0e1ecfad188c3a";
+
         public MainWindow()
         {
             InitializeComponent();
-            MessageBox.Show("teest");
-            MessageBox.Show("hijhgudsajf");
+        }
+
+        private async void Suchen_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string stadt = txtStadt.Text;
+            string apiUrl = $"https://api.weatherbit.io/v2.0/current?city={stadt}&key={apiKey}&lang=de";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        dynamic data = JObject.Parse(json);
+
+                        string beschreibung = data.data[0].weather.description;
+                        double temperatur = data.data[0].temp;
+
+                        txtErgebnis.Text = $"Beschreibung: {beschreibung}\nTemperatur: {temperatur}°C";
+                    }
+                    else
+                    {
+                        txtErgebnis.Text = "Fehler beim Abrufen der Daten.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                txtErgebnis.Text = "Fehler beim Abrufen der Daten.";
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
